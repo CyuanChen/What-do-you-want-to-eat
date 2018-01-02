68997a21536ba895cc6aae45e899e301068c5a89
Peter <peterchen302@gmail.com>
2018-01-02 10:54:38 +0800

    Add GoogleMap API

---
+++ Pods/Target Support Files/Pods-WhatDoYouWanTtoEat/Pods-WhatDoYouWanTtoEat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WhatDoYouWanTtoEat : NSObject
+@end
+@implementation PodsDummy_Pods_WhatDoYouWanTtoEat
+@end
--- WhatDoYouWanTtoEat/AppDelegate.swift
+++ WhatDoYouWanTtoEat/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        let apiKey: String = SecretKey.instance.getAPIKey()
+        GMSServices.provideAPIKey(apiKey)
+        GMSPlacesClient.provideAPIKey(apiKey)
+        
         return true
     }
 
